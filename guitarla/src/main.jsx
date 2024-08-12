524426a0069da524b949b7f5cb7eb0f66d9b614c
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-08-11 18:07:21 -0600

    Configuración inicial de la estructura de archivos de React.

--- guitarla/src/main.jsx
+++ guitarla/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App.jsx' // componente principal
+import './index.css' // hoja CSS principal
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render( // inyecta el código jsx y el CSS en el index.html en el div con el id "root"
   <StrictMode>
     <App />
   </StrictMode>,
