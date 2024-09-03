ca2532f6ebc4e1d58945bfdb725552199b28ea26
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-09-03 13:49:40 -0600

    Heredar y extender un type.

--- guitarla-ts/src/types/types.ts
+++ guitarla-ts/src/types/types.ts
@@ -6,11 +6,7 @@ export type TGuitar = {
     price : number;
 }
 
-export type TCardItem = {
-    id : number;
-    name : string;
-    image : string;
-    description : string;
-    price : number;
+// export interface ICardItem extends TGuitar { // extiende de TGuitar y agrega el campo quantity
+export type TCardItem = TGuitar & { // hereda de TGuitar y agrega el campo quantity
     quantity: number;
 }
